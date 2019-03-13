f667da5af7aa3970f21dfaf6bd0534f450166c50
Thanhnvsgu <32585608+Thanhnvsgu@users.noreply.github.com>
2019-03-13 11:57:29 +0700

    Update chat + video

--- PhongChat/main.js
+++ PhongChat/main.js
@@ -22,7 +22,10 @@ socket.on('Turn_off', user =>{
 var peer = new Peer({ key: 'lwjd5qra8257b9' }); // tạo ra 1 peer key mới
 
 peer.on('open', id => {
-    $("#videochat").append(`<video class="col-lg-6" id="video_${id}"> </video>`);
+    $("#videochat").append(`<div class="col-lg-6 id="div_${id}">                             
+                                <div> <video class="col-lg-12" controls poster="https://cdn2.vectorstock.com/i/1000x1000/81/96/france-flag-vector-20178196.jpg" id="video_${id}"> </video> </div> 
+                                <div id="user_video" style="text-align:center;color:#3399ff;font-weight:bold"> </div>
+                            </div>`);
     // console.log(id);
     $("#my-peer").append(id);
     
@@ -44,6 +47,7 @@ peer.on('open', id => {
         }
     });
     socket.on('Hello_user', e=>{
+        document.getElementById('user_video').innerHTML = e;
         document.getElementById('hello_user').innerHTML = `Xin chào, ${e}`;
         document.getElementById('btnlist').disabled = false;
     });
@@ -58,7 +62,10 @@ peer.on('open', id => {
     socket.on('New_user', user =>{
         $.notify(`${user.name} đang online`,'success');
     });
-
+    socket.on('connect_end', e=>{
+        alert(e);
+        $(`#div_${e}`).remove();
+    });
 
     $("#btncall").on('click',function(){
         
@@ -71,21 +78,25 @@ peer.on('open', id => {
                     // console.log(ten);                    
                     if(list[i].checked == true){             
                         // console.log(list[i].value);                      
-                        $("#videochat").append(`<video class="col-lg-3" id="video_${ten}"> </video>`);
-                        call.push(peer.call(`${ten}` , stream));
-                        
+                        $("#videochat").append(`<div class="col-lg-3" id="div_${ten}">                             
+                        <div> <video class="col-lg-12" controls poster="https://cdn1.vectorstock.com/i/1000x1000/44/90/italy-flag-on-a-white-vector-21844490.jpg" id="video_${ten}"> </video> </div> 
+                        <div id="user_video" style="text-align:center;color:#3399ff;font-weight:bold"> ${document.getElementById(`li_${ten}`).textContent} </div>
+                    </div>`);
+                        call.push(peer.call(`${ten}` , stream));                       
                     }
             };
-            
-            call.forEach(e => {
-                console.log(e);
-                e.on('stream',stream =>{
-                    playStream(`video_${e.peer}`,stream);
+            console.log(call.length);
+            call.forEach(t => {
+                console.log(t)  ;            
+                t.on('stream',stream => {
+                    playStream(`video_${t.peer}`,stream);
+                    alert("dang stream");
                 });
             });
-             
-           
-            // socket.emit('dungstream',stream);
+            $("#btncam_off").on('click',function(){
+                stream.getVideoTracks()[0].stop();
+                stream.getAudioTracks()[0].stop();
+            });
          }); 
          
     });
@@ -93,35 +104,60 @@ peer.on('open', id => {
     peer.on('call', function(call) {
         // alert("co nguoi goi");
         // console.log(call);
-        $("#videochat").append(`<video class="col-lg-3" id="video_${call.peer}"> </video>`);
+        $("#videochat").append(`<div class="col-lg-3" id = "div_${call.peer}">                             
+        <div> <video class="col-lg-12" controls poster="https://cdn1.vectorstock.com/i/1000x1000/44/90/italy-flag-on-a-white-vector-21844490.jpg" id="video_${call.peer}"> </video> </div> 
+        <div id="user_video" style="text-align:center;color:#3399ff;font-weight:bold"> ${document.getElementById(`li_${call.peer}`).textContent} </div>
+    </div>`);
         openStream().then(stream => {
             call.answer(stream);             
             playStream(`video_${id}`, stream);
             call.on('stream', remoteStream => {                
-                playStream(`video_${call.peer}`,remoteStream);        
+                playStream(`video_${call.peer}`,remoteStream);               
+                $("#btncam_off").on('click',function(){
+                    if(stream.getVideoTracks().length>0) stream.getVideoTracks()[0].stop();
+                    if(stream.getAudioTracks().length> 0 ) stream.getAudioTracks()[0].stop();
+                    console.log(`div_${call.peer}`);
+                    $(`#div_${call.peer}`).remove();
+                    socket.emit('close_connect',id);
+                });     
             });           
         });
-        
     });
+
+    $("#btnsend").on('click',function(){
+        socket.emit('data_chat', { data:document.getElementById('text_send').value,id:id });
+    });
+    socket.on('data_receive', e =>{
+        $("#chat_data").append(`<b>${e.ten}:</b> ${e.data} </br>`);
+    });
+
+    // var res;
+    // $("#btnuse").on('click',function(){
+    //     openStream().then(stream => {
+    //         alert('a');
+    //         playStream(`video_${id}`,stream);
+    //         $("#btnoff").on('click',function(){
+    //             stream.getTracks()[0].stop();
+    //             stream.getVideoTracks()[0].stop();
+    //         });           
+    //     });
+    // });
+    
 
  
 });
 
-function openStream(){
+function openStream()  {
     const config = {
-        "audio": false,
-        "video": {
-            "frameRate": {
-                "min": "30",
-                "max": "60"
-            }
-        }
-    };
+        video:true,
+        audio:false
+    }
     return navigator.mediaDevices.getUserMedia(config);
-}
+} 
 
 function playStream(idVideoTag, stream){
     const video = document.getElementById(idVideoTag);
     video.srcObject = stream;
     video.play();
 }
+
